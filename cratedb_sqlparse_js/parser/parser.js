1c4b3eca650bb96b9b5cc5a3f15f923db83fa814
surister <surister98@gmail.com>
2024-04-05 01:18:35 +0200

    Implement ExceptionErrorListener

--- cratedb_sqlparse_js/parser/parser.js
+++ cratedb_sqlparse_js/parser/parser.js
@@ -1,26 +1,36 @@
 import SqlBaseLexer from "./generated_parser/SqlBaseLexer.js";
 import SqlBaseParser from "./generated_parser/SqlBaseParser.js";
-import {CommonTokenStream, InputStream, Token} from "antlr4";
+import {CommonTokenStream, ErrorListener, InputStream, Token} from "antlr4";
+
+class ParsingError extends Error {
+
+}
 
 class CaseInsensitiveStream extends InputStream {
     LA(offset) {
         const result = super.LA(offset);
-        if (result <= 0 || result === Token.EOF){
+        if (result <= 0 || result === Token.EOF) {
             return result;
         }
-        return String.fromCharCode(result).toUpperCase().charCodeAt();
+        return String.fromCharCode(result).toUpperCase().charCodeAt(0);
+    }
+}
+
+class ExceptionErrorListener extends ErrorListener {
+    syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
+        throw new ParsingError(`line${line}:${column} ${msg}`);
     }
 }
 
 export function sqlparse(query) {
     const input = new CaseInsensitiveStream(query);
     const lexer = new SqlBaseLexer(input);
-    // lexer.removeErrorListeners()
+    lexer.removeErrorListeners();
     const stream = new CommonTokenStream(lexer);
 
     const parser = new SqlBaseParser(stream);
-    // parser.removeErrorListeners()
-    // parser.addErrorListener(ExceptionErrorListener())
+    parser.removeErrorListeners();
+    parser.addErrorListener(new ExceptionErrorListener());
 
     const tree = parser.statements();
 
