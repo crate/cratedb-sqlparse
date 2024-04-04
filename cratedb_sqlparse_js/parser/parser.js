8163f1f16a5d0799633796427ddcb5325d6a8145
surister <surister98@gmail.com>
2024-04-05 01:10:49 +0200

    Implement CaseInsensitiveStream

--- cratedb_sqlparse_js/parser/parser.js
+++ cratedb_sqlparse_js/parser/parser.js
@@ -1,18 +1,29 @@
 import SqlBaseLexer from "./generated_parser/SqlBaseLexer.js";
 import SqlBaseParser from "./generated_parser/SqlBaseParser.js";
-import {CommonTokenStream, InputStream} from "antlr4";
+import {CommonTokenStream, InputStream, Token} from "antlr4";
+
+class CaseInsensitiveStream extends InputStream {
+    LA(offset) {
+        const result = super.LA(offset);
+        if (result <= 0 || result === Token.EOF){
+            return result;
+        }
+        return String.fromCharCode(result).toUpperCase().charCodeAt();
+    }
+}
 
 export function sqlparse(query) {
-    const input = new InputStream(query)
-    const lexer = new SqlBaseLexer(input)
+    const input = new CaseInsensitiveStream(query);
+    const lexer = new SqlBaseLexer(input);
     // lexer.removeErrorListeners()
-    const stream = new CommonTokenStream(lexer)
+    const stream = new CommonTokenStream(lexer);
 
-    const parser = new SqlBaseParser(stream)
+    const parser = new SqlBaseParser(stream);
     // parser.removeErrorListeners()
     // parser.addErrorListener(ExceptionErrorListener())
 
-    const tree = parser.statements()
-    return tree.children
+    const tree = parser.statements();
+
+    return tree.children;
 
 }
