f01767a9544276bd025c58fdcddf06e14fc2091e
Andreas Motl <andreas.motl@crate.io>
2024-05-30 10:04:39 +0200

    JavaScript: Naming things
    The package intends to be published as `@cratedb/cratedb-sqlparse`.

--- cratedb_sqlparse_js/vite.config.js
+++ cratedb_sqlparse_js/vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
     build: {
         lib: {
             // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'parser/index.js'),
-            name: 'cratedb_sqlparse', // the proper extensions will be added
+            entry: resolve(__dirname, 'cratedb_sqlparse/index.js'),
+            name: '@cratedb/cratedb-sqlparse', // the proper extensions will be added
             fileName: 'sqlparse',
         }, rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
