fccf2bc0fe863f2da00ce5fd206408866b93ad14
surister <surister98@gmail.com>
2024-06-11 19:02:44 +0200

    Add `with_properties` and `interpolated_properties`

--- cratedb_sqlparse_py/tests/test_enricher.py
+++ cratedb_sqlparse_py/tests/test_enricher.py
@@ -44,3 +44,32 @@ def test_table_name_statements():
 
     assert stmts[3].metadata.schema is None
     assert stmts[3].metadata.table_name == "tbl1"
+
+
+def test_table_with_properties():
+    from cratedb_sqlparse import sqlparse
+
+    query = "CREATE TABLE tbl (A TEXT) WITH ('key' = 'val', 'key2' = 2, 'key3' = true)"
+
+    stmt = sqlparse(query)[0]
+    keys = ["key", "key2"]
+
+    assert all(x in stmt.metadata.with_properties for x in keys)
+    assert stmt.metadata.with_properties["key"] == "val"
+    assert stmt.metadata.with_properties["key2"] == "2"
+    assert stmt.metadata.with_properties["key3"] == "true"
+
+
+def test_with_with_interpolated_properties():
+    from cratedb_sqlparse import sqlparse
+
+    query = "CREATE TABLE tbl (A TEXT) WITH ('key' = $1, 'key2' = '$2')"
+
+    stmt = sqlparse(query)[0]
+    keys = ["key", "key2"]
+
+    # Has all the keys.
+    assert all(x in stmt.metadata.interpolated_properties for x in keys)
+    assert all(x in stmt.metadata.with_properties for x in keys)
+    assert stmt.metadata.with_properties["key"] == "$1"
+    assert stmt.metadata.with_properties["key2"] == "$2"
