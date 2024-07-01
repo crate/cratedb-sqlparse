70df187b27be351cceb0635ca1921f6f321dcfa5
surister <surister98@gmail.com>
2024-07-01 17:22:38 +0200

    Set CrateDB version to 5.7.2

--- setup_grammar.py
+++ setup_grammar.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
     setup_logging()
 
     input_target = sys.argv[1]
-    version = '5.6.4'
+    version = '5.7.2'
 
     if input_target.startswith("py"):
         target = Antlr4Target.python
