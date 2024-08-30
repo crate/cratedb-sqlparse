e14027ef4c28bdbdfbfc5de72c7ba081df9ed086
surister <surister98@gmail.com>
2024-08-30 12:57:48 +0200

    Javascript: Export `ParserError` and `Statement`

--- cratedb_sqlparse_js/cratedb_sqlparse/index.js
+++ cratedb_sqlparse_js/cratedb_sqlparse/index.js
@@ -1,3 +1,3 @@
-import {sqlparse} from "./parser.js";
-export {sqlparse};
+import {sqlparse, ParseError, Statement} from "./parser.js";
+export {sqlparse, ParseError, Statement};
 export const __cratedb_version__ = "5.7.2"
