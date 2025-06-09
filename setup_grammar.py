38b265e89d08298e57041f6e0e715adf1e506cc6
Ivan <surister98@gmail.com>
2025-06-09 11:40:48 +0200

    Set CrateDB to 5.10.7
    This is the latest available version https://github.com/crate/docker-crate/blob/45ab56012a7f2d38b9150238a2e2fe617b1189e3/Dockerfile

--- setup_grammar.py
+++ setup_grammar.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
     setup_logging()
 
     input_target = sys.argv[1]
-    version = '5.10.4'
+    version = '5.10.7'
 
     if input_target.startswith("py"):
         target = Antlr4Target.python
